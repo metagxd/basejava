f0817a6a9d3570a2de6be939634dc7b1c347e78a
Artyom Vasilyev <metagxd@gmail.com>
2020-03-01 18:30:57 +0300

    HW4 add reflection

--- src/com/basejava/webapp/storage/MainReflection.java
+++ src/com/basejava/webapp/storage/MainReflection.java
@@ -1,5 +1,27 @@
 package com.basejava.webapp.storage;
 
-public class MainReflection {
+import com.basejava.webapp.model.Resume;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException {
+        Resume r = new Resume();
+        Field field = r.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(r));
+        field.set(r, "new_uuid");
+        // TODO : invoke r.toString via reflection
+        System.out.println(r);
+        Method[] methods = r.getClass().getDeclaredMethods();
+        for (Method method : methods) {
+            System.out.println(method);
+        }
+        System.out.println(methods[1].invoke(r));
+
+    }
 }
