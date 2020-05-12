1a3d6be41572dfbfcd4ca1db5336809f95bd700c
Artyom Vasilyev <metagxd@gmail.com>
2020-05-12 20:05:26 +0300

    HW12 implement oddOrEven

--- src/com/basejava/webapp/MainStream.java
+++ src/com/basejava/webapp/MainStream.java
@@ -1,12 +1,16 @@
 package com.basejava.webapp;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MainStream {
 
     public static void main(String[] args) {
         int[] values = new int[]{1, 3, 1, 1, 1, 1, 1, 3, 2, 3, 1, 1, 1, 3, 3, 3, 3, 3, 2, 2, 1, 3};
+        int[] values2 = new int[]{1, 4, 8, 6, 9, 3, 0};
         System.out.println(minValue(values));
+        System.out.println(oddOrEven(values2));
     }
 
     static int minValue(int[] values) {
@@ -17,4 +21,18 @@ public class MainStream {
                 .orElse(0);
     }
 
+    static List<Integer> oddOrEven(int[] values) {
+        if (Arrays.stream(values)
+                .sum() % 2 == 0) {
+            return Arrays.stream(values)
+                    .sorted()
+                    .filter(x -> x % 2 == 0)
+                    .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+        } else {
+            return Arrays.stream(values)
+                    .filter(x -> x % 2 != 0)
+                    .sorted()
+                    .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+        }
+    }
 }
