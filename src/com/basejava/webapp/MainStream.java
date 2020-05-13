8d0ca8fe15c1d7f4d7da3c473588c91e34d8e3ee
Artyom Vasilyev <metagxd@gmail.com>
2020-05-13 08:06:04 +0300

    simplify oddOrEven HW13

--- src/com/basejava/webapp/MainStream.java
+++ src/com/basejava/webapp/MainStream.java
@@ -8,7 +8,7 @@ public class MainStream {
 
     public static void main(String[] args) {
         int[] values = new int[]{1, 3, 1, 1, 1, 1, 1, 3, 2, 3, 1, 1, 1, 3, 3, 3, 3, 3, 2, 2, 1, 3};
-        int[] values2 = new int[]{1, 4, 8, 6, 9, 3, 0};
+        int[] values2 = new int[]{1, 4, 8, 6, 9, 3, 1};
         System.out.println(minValue(values));
         System.out.println(oddOrEven(values2));
     }
@@ -23,14 +23,11 @@ public class MainStream {
 
     static List<Integer> oddOrEven(int[] values) {
         int sumOfStream = Arrays.stream(values).sum();
+        System.out.println(sumOfStream % 2);
 
-        if (sumOfStream % 2 == 0) {
-            return Arrays.stream(values)
-                    .filter(x -> x % 2 == 0)
-                    .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
-        }
         return Arrays.stream(values)
-                .filter(x -> x % 2 != 0)
+                .filter((x) -> (sumOfStream % 2 != 0) && (x % 2 != 0) || (sumOfStream % 2 == 0) && (x % 2 == 0))
                 .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
+
     }
 }
